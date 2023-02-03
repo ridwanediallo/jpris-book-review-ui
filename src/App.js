4c2bc6dd9a2beca389c37f4a2d99d92d2fad7225
ridwan <ridwanediallo99@gmail.com>
2023-02-03 20:42:45 +0000

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './components/HomePage';
+import Signup from './components/features/authentication/Signup';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Book review</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Book review</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Signin />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
