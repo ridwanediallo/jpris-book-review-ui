e2c1d0ab0c965b2bda64551f48e6ef50b643c686
ridwan <ridwanediallo99@gmail.com>
2023-02-04 00:08:08 +0000

    Update the name of the button on the signup form

--- src/components/features/authentication/Signup.js
+++ src/components/features/authentication/Signup.js
@@ -109,7 +109,7 @@ const Signup = () => {
             type="submit"
             className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
           >
-            Button
+            Signup
           </button>
         </div>
       </div>
